55c4e2b1cc3ce2d0b8e8612beec1187f111c1768
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2012-11-10 21:43:20 -0200

    Moving directories. Adding some data.

--- geradados.m
+++ geradados.m
@@ -1,34 +1,38 @@
-%
-% dados = geradados(fisCaminhaoMandani, intervaloX, intervaloPHI)
+% data = geradados(fisFilename)
 %
 % Gera dados para o caminhao em formato de matriz
 % a partir de um FIS Mamdani.
 %
 % Parametros: quantidade de intervalos para cada vari�vel fuzzy
+function geradados(fisFilename)
+    if nargin < 3
+        fisFilename = readfis('fis/caminhao-default.fis');
+    end
 
-function data = geradados(fisCaminhaoMandani, intervaloX, intervaloPHI)
+    %gera a lista de pares de entrada [X, PHI]
+    [X, PHI] = meshgrid( ...
+        linspace(0, 100), ...
+        linspace(-90, 270, 360) ...
+    );
+    entradas = [X(:) PHI(:)];
 
-if nargin < 3
-    fisCaminhaoMandani = readfis('caminhao');
-    intervaloX = 10;
-    intervaloPHI = 10;
-    
-    fprintf('Parametros default: "caminhao.fis", 10, 10');
+    %gera matriz com dados de entrada e sa�da [X PHI Saida]
+    saidas = evalfis(entradas, fisFilename);
+    data = [entradas saidas];
+
+    %guarda os dados num arquivo, para que todos possam usá-lo.
+    filename = get_output_file_name();
+    csvwrite(filename, data);
 end
 
-%gera valores, dada a quantidade de intervalos especificada
-% vou dar espaco maior para o caminhao
-%intervalosX = linspace(0 +1, 100 -1, intervaloX);
-%intervalosPHI = linspace(-105 +1, 285 -1, intervaloPHI);
-intervalosX = linspace(0 + intervaloX, 100 - intervaloX, intervaloX);
-intervalosPHI = linspace(-105 + intervaloPHI, 285 - intervaloPHI, intervaloPHI);
 
 
-
-%gera a lista de pares de entrada [X, PHI]
-[X, PHI] = meshgrid(intervalosX, intervalosPHI);
-entradas = [X(:) PHI(:)];
-
-%gera matriz com dados de entrada e sa�da [X PHI Saida]
-saidas = evalfis(entradas, fisCaminhaoMandani);
-data = [entradas saidas];
+function name = get_output_file_name()
+%Função utilitária que cria um nome variável para as saídas produzidas.
+    the_time = clock();
+    username = getenv('USERNAME');
+    name = ['data/' username '-' ...
+                num2str(the_time(1)) '-' num2str(the_time(2)) '-' ...
+                num2str(the_time(3)) '-' num2str(the_time(4)) '-' ...
+                num2str(the_time(5)) '-' num2str(the_time(6)) '.csv'];
+end
