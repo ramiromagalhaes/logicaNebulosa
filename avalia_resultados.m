dc6bb09004e151c5c4beea9e536b9e3d9437df63
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2012-11-26 22:37:34 -0200

    Removendo comentário inútil.

--- avalia_resultados.m
+++ avalia_resultados.m
@@ -15,17 +15,4 @@ function estatisticas = avalia_resultados( resultado )
         estatisticas(3, i) = var ( resultado(:,i) );
     end
 
-    %{
-    %Se algum dia alguém quiser escrever em arquivo as estatísticas, é só
-    usar o seguinte código:
-
-    for i = 1:length(estatisticas(:,1))
-        fprintf(fd,'%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\n', ...
-            estatisticas(i, 1), estatisticas(i, 2), estatisticas(i, 3), estatisticas(i, 4), ...
-            estatisticas(i, 5), estatisticas(i, 6), estatisticas(i, 7), estatisticas(i, 8), ...
-            estatisticas(i, 9), estatisticas(i, 10), estatisticas(i, 11), estatisticas(i, 12), ...
-            estatisticas(i, 13), estatisticas(i, 14) );
-    end
-    %}
-
 end
